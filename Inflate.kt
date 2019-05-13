567d66bec5fccae1d14af303833421fa4aa3554d
Nier <fgd1014074315@gmail.com>
2019-05-13 22:29:39 +0800

    修改解析类

--- Inflate.kt
+++ Inflate.kt
@@ -4,9 +4,8 @@ import java.io.File
 import java.io.RandomAccessFile
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import java.nio.channels.FileChannel
 import java.util.zip.GZIPInputStream
-import java.util.zip.GZIPOutputStream
-import kotlin.experimental.and
 
 /**
  * Created by fgd
@@ -23,63 +22,45 @@ class Gzip {
         val cacheFile = File("./cache.mmap")
         val temp = RandomAccessFile(cacheFile, "r")
         val fileChannel = temp.channel
-
-        val tempBuffer = ByteBuffer.allocate(1)
-        val fileSize = fileChannel.size()
-
-        var start = 0L
-//        var end = 0L
-
+        val headTagBuffer = ByteBuffer.allocate(1)
         val headTag: Byte = 1
-//        val endTag: Byte = 16
 
-
-        for (i in 0..(fileSize - 1)) {
-            fileChannel.read(tempBuffer, i)
-            if (tempBuffer.get(0) == headTag) {
-                start = i
-                break
+        for (i in 0..(fileChannel.size() - 1)) {
+            fileChannel.read(headTagBuffer, i)
+            if (headTagBuffer.get(0) == headTag) {
+                headTagBuffer.clear()
+                println("tartget Index  start >>> $i")
+                parse(fileChannel, i)
             }
-            tempBuffer.clear()
+            headTagBuffer.clear()
         }
-        println("tartget Index  start >>> $start")
-        tempBuffer.clear()
+    }
 
 
+    fun parse(fileChannel: FileChannel, headTagIndex: Long) {
+        var contentIndex = 0L
         val lengthBuffer = ByteBuffer.allocate(4)
-        fileChannel.read(lengthBuffer, start + 1)
+        fileChannel.read(lengthBuffer, headTagIndex + 1)
         lengthBuffer.order(ByteOrder.BIG_ENDIAN)
         val length = lengthBuffer.getInt(0)
         println("content length >>> $length")
-
-//        for (i in 0..(fileSize - 1)) {
-//            fileChannel.read(tempBuffer, i)
-//            if (tempBuffer.get(0) == endTag) {
-//                end = i
-//                break
-//            }
-//            tempBuffer.clear()
-//        }
-//        println("tartget Index  end >>> $end")
-//        tempBuffer.clear()
-
-        if (start != 0L) {
-            start += 5L
+        if (length < 0) {
+            println("!!! content length >>> $length")
+            return
+        }
+        if (headTagIndex != 0L) {
+            contentIndex = headTagIndex + 5L
         }
 
-        var contentLength = length
-        val contentBuffer = ByteBuffer.allocate(contentLength)
+        val contentBuffer = ByteBuffer.allocate(length)
 
-        fileChannel.read(contentBuffer, start)
-        var byteIn = ByteArrayInputStream(contentBuffer.array())
+        fileChannel.read(contentBuffer, contentIndex)
+        val byteIn = ByteArrayInputStream(contentBuffer.array())
 
+        val gzipIn = GZIPInputStream(byteIn)
 
-//        println("result >>>>>" + GZIPUtils.uncompressToString(contentBuffer.array()))
+        val byteOut = ByteArrayOutputStream()
 
-        var gzipIn = GZIPInputStream(byteIn)
-
-        var byteOut = ByteArrayOutputStream()
-//        var gzipOut = GZIPOutputStream(byteOut)
         val buffer = ByteArray(256)
         var n: Int
 
@@ -89,17 +70,10 @@ class Gzip {
             byteOut.write(buffer, 0, n)
             n = gzipIn.read(buffer)
         }
-//        do {
-//            n = gzipIn.read(buffer)
-//            byteOut.write(buffer, 0, n)
-//        } while (n >= 0)
 
-
-        println("byte out >>> ${String(byteOut.toByteArray())}")
+        println("byte out start with $headTagIndex >>> ${String(byteOut.toByteArray())}")
 
         byteOut.close()
-//        gzipIn.close()
-        fileChannel.close()
     }
 
 }
