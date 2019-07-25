9ec2b310632a85545376a18c8a4b5b56916715cb
Nier <fgd1014074315@gmail.com>
2019-07-25 18:18:10 +0800

    update readme

--- app/src/main/java/com/nier/sample/MainActivity.kt
+++ app/src/main/java/com/nier/sample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.nier.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
 import com.nier.nlogger.NLogger
 import com.nier.nlogger.R
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,22 +18,22 @@ class MainActivity : AppCompatActivity() {
 
         NLogger.init(this)
 
-        findViewById<EditText>(R.id.tv_result).setText("Hello World QWERTYUIOPASDFGHJKL[]{};':ZXCVBNM<>?,./123456789!@#\\\$%^*()!")
-
-        findViewById<TextView>(R.id.btn_init).setOnClickListener {
-            //            NLogger.init(this)
-            testMutiThread(50)
-        }
-
-        findViewById<TextView>(R.id.btn_write).setOnClickListener {
-            val text = findViewById<EditText>(R.id.tv_result).text
-            NLogger.write("date[${System.currentTimeMillis()}] content[$text]")
-
-        }
-
-        findViewById<TextView>(R.id.btn_flush).setOnClickListener {
-            NLogger.flush()
-        }
+//        findViewById<EditText>(R.id.tv_result).setText("Hello World QWERTYUIOPASDFGHJKL[]{};':ZXCVBNM<>?,./123456789!@#\\\$%^*()!")
+//
+//        findViewById<TextView>(R.id.btn_init).setOnClickListener {
+//            //            NLogger.init(this)
+//            testMutiThread(50)
+//        }
+//
+//        findViewById<TextView>(R.id.btn_write).setOnClickListener {
+//            val text = findViewById<EditText>(R.id.tv_result).text
+//            NLogger.write("date[${System.currentTimeMillis()}] content[$text]")
+//
+//        }
+//
+//        findViewById<TextView>(R.id.btn_flush).setOnClickListener {
+//            NLogger.flush()
+//        }
 
 
 //        NLoggerProxy.instance.init(this)
@@ -39,6 +42,24 @@ class MainActivity : AppCompatActivity() {
 //        NLoggerProxy.instance.write("")
 //        NLoggerProxy.instance.write("")
 //        NLoggerProxy.instance.flush()
+        testCoroutine()
+    }
+
+    private fun testCoroutine() {
+        val TAG = "fgd"
+        Log.d(TAG, "Coroutine start")
+        val test1 = runBlocking {
+            coroutineScope {
+                launch {
+                    delay(3000)
+                    Log.d(TAG, "Coroutine coroutineScope launch finish ## currentThread().name >>> ${Thread.currentThread().name}")
+                }
+                Log.d(TAG, "Coroutine coroutineScope ## currentThread().name >>> ${Thread.currentThread().name}")
+            }
+
+            Log.d(TAG, "Coroutine runBlocking finish")
+        }
+        Log.d(TAG, "testCoroutine finish")
     }
 
 
